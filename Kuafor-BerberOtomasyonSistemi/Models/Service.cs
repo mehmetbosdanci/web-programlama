e1991b2cf27d5cb78bd3f62e9176e7a008316057
mehmetbosdanci <mehmet.bosdanci@ogr.sakarya.edu.tr>
2024-12-21 13:52:40 +0300

    Service modeli eklendi

---
+++ Kuafor-BerberOtomasyonSistemi/Models/Service.cs
@@ -0,0 +1,10 @@
+﻿namespace Kuafor_BerberOtomasyonSistemi.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int DurationInMinutes { get; set; }
+    }
+}
